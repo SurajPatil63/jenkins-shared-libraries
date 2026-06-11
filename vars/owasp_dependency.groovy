c448ab5a4a18b8db3976f4aad997a836eb9998ca
SurajPatil63 <147255824+SurajPatil63@users.noreply.github.com>
2026-06-12 01:32:23 +0530

    owasp_dependency.groovy

---
+++ vars/owasp_dependency.groovy
@@ -0,0 +1,4 @@
+def call() {
+    dependencyCheck additionalArguments: '--scan ./ --format XML --out ./ --prettyPrint', odcInstallation: 'DC'
+    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+}
